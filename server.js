d1e221fc5088c095adb4e14132ffa4bbf19a5f9a
Patrick Chan <q_p18@hotmail.com>
2020-10-30 18:50:41 +0000

    Complete chat server levels 1 - 3

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(cors());
 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const welcomeMessage = {
   id: 0,
   from: "Bart",
@@ -20,4 +24,70 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+// create a message
+app.use(bodyParser.json());
+app.post("/messages", function (req, res) {
+
+  const newMessage = req.body;
+  // console.log(Object.values(newMessage));
+  // console.log(newMessage.text);
+
+  if(newMessage.from == "" ||  newMessage.text == "") {
+    
+    res.status(400);
+    res.json("Please enter all fields");
+
+  } else {
+
+    newMessage.id = messages.length;
+
+    messages.push(newMessage);
+
+    res.status(201);
+    res.json("New message added successfully");
+  }
+});
+
+// Read all messages
+app.get("/messages", function (req, res) {
+  res.json(messages);
+})
+
+//  [ ] Read _only_ messages whose text contains a given substring: `/messages/search?text=express`
+app.get("/messages/search", function (req, res) {
+  const {text} = req.query;
+  const textFiltered = messages.filter( message => message.text.toLowerCase().includes(text) );
+  console.log(textFiltered);
+  res.json(textFiltered);
+})
+
+//  [ ] Read only the most recent 10 messages: `/messages/latest`
+app.get("/messages/latest", function (req,res) {
+  if(messages.length > 10) {
+    let surplusItems = messages.length - 10;
+    let latest = messages.slice(surplusItems);
+    res.json(latest);
+  } else {
+    res.json(messages);
+  }
+})
+
+// Read one message specified by an ID
+app.get("/messages/:id", function (req, res) {
+  let {id} = req.params;
+  let messageFiltered = messages.filter(message => message.id == id);
+  res.json(messageFiltered);
+})
+
+// Delete a message, by ID
+app.delete("/messages/:id", function (req, res) {
+  const {id} = req.params;
+  messages.forEach(message => {
+    if(message.id == id) {
+      messages.splice(id, 1)
+    }
+  })
+  res.json("Message " + id + " deleted");
+});
+
+app.listen(3000);
